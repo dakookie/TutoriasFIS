ae4d61f938be374b471b24f7fb3d2a5a3a06ba39
dakookie <damaris.suquillo@epn.edu.ec>
2026-01-27 23:40:33 -0500

    Fix app.controller.ts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+// 1. Importar el decorador Public
+import { Public } from './common/decorators/public.decorator'; 
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // 2. Añadir el decorador aquí para saltar la seguridad
+  @Public() 
   @Get()
   getHello(): string {
     return this.appService.getHello();
